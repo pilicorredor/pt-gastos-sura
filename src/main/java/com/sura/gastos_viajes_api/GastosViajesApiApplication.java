bda9b49c4112754934a73142e13c66b1901332cb
Pilar Andrea Corredor Corredor <pcorredor@sura.com.co>
2025-07-08 09:49:43 -0500

    Inicialización del proyecto con estructura base de Spring Boot

---
+++ src/main/java/com/sura/gastos_viajes_api/GastosViajesApiApplication.java
@@ -0,0 +1,13 @@
+package com.sura.gastos_viajes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastosViajesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastosViajesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sura/gastos_viajes_api/GastosViajesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sura.gastos_viajes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastosViajesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
